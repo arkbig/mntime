22458cfa462a184a4a2d74fc8b5f974e1dede563
big@m1 <big@m1.local>
2022-09-04 11:18:53 +0900

    🩹Set the name_value of options

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@ use clap::{AppSettings, Parser};
 
 /// Command Line Args
 // TODO: I want to embed package.name in the about document.
-// TODO: I want to change the value name of an option. e.g. -w, --warmup <NUM>
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None, setting = AppSettings::TrailingVarArg)]
 struct CliArgs {
     /// Perform exactly NUM runs for each command.
-    #[clap(short, long, value_parser, default_value_t = 5)]
+    #[clap(short, long, value_parser, value_name = "NUM", default_value_t = 5)]
     runs: u16,
 
     /// Time command used.
-    #[clap(short='T', long, value_parser, default_value = "gtime")]
+    #[clap(short='T', long, value_parser, value_name = "COMMAND", default_value = "gtime")]
     time_command: String,
 
     /// Arguments of the time command used.
-    #[clap(short, long, value_parser, default_value = "")]
+    ///
+    /// Quoting if flag is included or there are multiple args.
+    #[clap(short, long, value_parser, value_name = "ARGS", default_value = "")]
     time_args: String,
 
     /// The commands to benchmark.
